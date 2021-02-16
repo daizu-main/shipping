f25d796bccdd99e9e2be65cc3e2975225f8a5349
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-02-16 08:49:20 +0100

    add preview component for DHL events

---
+++ schemas/components/dhlPreview.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import { GiFactory, GiTruck, GiCheckMark, GiMailbox } from "react-icons/gi"
+import { BsFillExclamationTriangleFill } from "react-icons/bs"
+import { FaQuestion, FaDhl } from "react-icons/fa"
+
+const formatDate = (inputDate) => {
+  const [datePart, timePart] = inputDate.split("T")
+  const outputDate = datePart.split("-").reverse().join(".")
+  const outputTime = timePart.split(":").slice(0, 2).join(":")
+  return `${outputDate}, ${outputTime}`
+}
+
+const getRedirected = (str) => {
+  return str.includes("The shipment is being brought to")
+}
+
+const getPackstation = (str) => {
+  return (
+    str.includes("The shipment is ready for pick-up at the") &&
+    str.includes("PACKSTATION")
+  )
+}
+
+const StatusSymbol = ({ status }) => {
+  switch (status) {
+    case "delivered":
+      return <GiCheckMark size="3em" style={{ minWidth: "50px" }} />
+    case "packstation":
+      return <GiMailbox size="3em" style={{ minWidth: "50px" }} />
+    case "redirect":
+      return <FaDhl size="3em" style={{ minWidth: "50px" }} />
+    case "pre-transit":
+      return <GiFactory size="3em" style={{ minWidth: "50px" }} />
+    case "transit":
+      return <GiTruck size="3em" style={{ minWidth: "50px" }} />
+    case "failure":
+      return (
+        <BsFillExclamationTriangleFill
+          size="3em"
+          style={{ minWidth: "50px" }}
+        />
+      )
+    default:
+      return <FaQuestion size="3em" style={{ minWidth: "50px" }} />
+  }
+}
+
+export default class dhlPreview extends Component {
+  static propTypes = { value: PropTypes.object }
+  render() {
+    const { time, status, statusCode } = this.props.value
+    const formattedTime = formatDate(time)
+    const redirected = getRedirected(status)
+    const packstation = getPackstation(status)
+    const lastStatusCode = packstation
+      ? "packstation"
+      : redirected
+      ? "redirect"
+      : statusCode
+    return (
+      <div
+        style={{
+          minHeight: "2em",
+          border: "2px grey",
+          padding: "5px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        {lastStatusCode && <StatusSymbol status={lastStatusCode} />}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            paddingLeft: "10px",
+          }}
+        >
+          <div
+            style={{
+              fontWeight: 400,
+              fontSize: "0.9em",
+              color: "#111111",
+              marginTop: 0,
+              marginBottom: 0,
+              paddingBottom: 0,
+              paddingTop: 0,
+            }}
+            title={status}
+            dangerouslySetInnerHTML={{ __html: status }}
+          />
+          <p
+            style={{
+              fontWeight: 300,
+              fontSize: "0.8em",
+              color: "#333333",
+              marginTop: 0,
+              marginBottom: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+            }}
+          >
+            {formattedTime}
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
--- schemas/dhlEvent.js
+++ schemas/dhlEvent.js
@@ -1,43 +1,4 @@
-import { GiFactory, GiTruck, GiCheckMark, GiMailbox } from "react-icons/gi"
-import { BsFillExclamationTriangleFill } from "react-icons/bs"
-import { FaQuestion, FaDhl } from "react-icons/fa"
-
-const getRedirected = (str) => {
-  return str.includes("The shipment is being brought to")
-}
-
-const getPackstation = (str) => {
-  return (
-    str.includes("The shipment is ready for pick-up at the") &&
-    str.includes("PACKSTATION")
-  )
-}
-
-const getIcon = (lastStatus) => {
-  switch (lastStatus) {
-    case "delivered":
-      return GiCheckMark
-    case "packstation":
-      return GiMailbox
-    case "redirect":
-      return FaDhl
-    case "pre-transit":
-      return GiFactory
-    case "transit":
-      return GiTruck
-    case "failure":
-      return BsFillExclamationTriangleFill
-    default:
-      return FaQuestion
-  }
-}
-
-const formatDate = (inputDate) => {
-  const [datePart, timePart] = inputDate.split("T")
-  const outputDate = datePart.split("-").reverse().join(".")
-  const outputTime = timePart.split(":").slice(0, 2).join(":")
-  return `${outputDate}, ${outputTime}`
-}
+import dhlPreview from "./components/dhlPreview"
 
 export default {
   name: "dhlEvent",
@@ -69,23 +30,12 @@ export default {
       statusCode: "statusCode",
     },
     prepare({ time, status, statusCode }) {
-      const redirected = getRedirected(status)
-      const packstation = getPackstation(status)
-      const lastStatusCode = packstation
-        ? "packstation"
-        : redirected
-        ? "redirect"
-        : statusCode
-      const icon = getIcon(lastStatusCode)
-      const [dateInput, clockInput] = time.split("T")
-      const date = dateInput.split("-").reverse().join(".")
-      const clock = clockInput.split(":").slice(0, 2).join(":")
-      const timeOutput = formatDate(time)
       return {
-        title: status,
-        subtitle: timeOutput,
-        media: icon,
+        time,
+        status,
+        statusCode,
       }
     },
+    component: dhlPreview,
   },
 }
